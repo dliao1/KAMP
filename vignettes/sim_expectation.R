8566bcbe4a441aa44b42a7a12ce1cc7a1c2b67bc
dliao1 <55068812+dliao1@users.noreply.github.com>
2026-02-24 01:13:14 -0500

    full reps and perms

--- vignettes/sim_expectation.R
+++ vignettes/sim_expectation.R
@@ -20,8 +20,8 @@ clust_values <- c(TRUE, FALSE)
 correction <- c("trans", "iso")
 univariate <- FALSE
 seed_start = 500
-n_rep <- 2 # just in case... tot est my variance addition works
-nperm <- 2
+n_rep <- 100 # just in case... tot est my variance addition works
+nperm <- 1000
 
 
 param_grid <- expand.grid(n = n_values,
